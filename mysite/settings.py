9ab5d94ab8483a5c6989e02bf363e969e20b520a
Julia <julia.schmidt1988@yahoo.de>
2017-10-15 11:39:50 +0200

    Meine Django Girls App, erster Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9#l1vz(6qgaap^m*jt^aeg)g#din)+f=)o5#xnno!7n+zj0c7g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juliwu.pythonanywhere.com']
 
 
 # Application definition
